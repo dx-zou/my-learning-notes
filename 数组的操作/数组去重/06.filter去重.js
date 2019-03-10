dadfd04ef383f071e99388f604e3483360acbd87
zoufeng <casafo@163.com>
2019-03-10 17:07:50 +0800

    数组去重的方法

--- 数组的操作/数组去重/06.filter去重.js
+++ 数组的操作/数组去重/06.filter去重.js
@@ -10,10 +10,8 @@
 
 
 Array.prototype.unique = function(){
-    return this.filter((item,index) => {
-        return this.indexOf(item) === index;
-    })
+    return this.filter((item,index) => this.indexOf(item) === index)
 };
-
-var arr = [1,1,2,2,3,'3','a','a','bbb','bbb'];
+// var arr = [1,1,3,'3','a','a',{},{},NaN,NaN,undefined,undefined,[1,2],[1,2]];
+var arr = [1,1,'2','2','NaN','NaN']
 console.log(arr.unique());
