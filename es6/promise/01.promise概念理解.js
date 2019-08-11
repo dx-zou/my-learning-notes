85d97ced6a1b2738237844b6ca7bab0c0ae69c57
zoufeng <casafo@163.com>
2019-08-11 18:13:41 +0800

    node.js 文件模块学习

--- es6/promise/01.promise概念理解.js
+++ es6/promise/01.promise概念理解.js
@@ -1,21 +1,21 @@
-const FS = require('fs')
-const PATH = require('path')
+const FS = require("fs");
+const PATH = require("path");
 function readFileByPromise(fpath) {
-    return new Promise((resolve, reject) => {
-        FS.readFile(PATH.join(__dirname,fpath),'utf-8',function(err,data) {
-            if (err) return reject(err)
-            resolve(data)
-        })
+  return new Promise((resolve, reject) => {
+    FS.readFile(PATH.join(__dirname, fpath), "utf-8", function(err, data) {
+      if (err) return reject(err);
+      resolve(data);
     });
+  });
 }
-readFileByPromise('./promise.txt')
-.then(res => {
-    console.log(res)
-    return readFileByPromise('./promise1.txt')
-})
-.then(res => {
-    console.log(res)
-})
-.catch(err => {
-    console.log(err.message)
-})
+readFileByPromise("./promise.txt")
+  .then(res => {
+    console.log(res);
+    return readFileByPromise("./promise1.txt");
+  })
+  .then(res => {
+    console.log(res);
+  })
+  .catch(err => {
+    console.log(err.message);
+  });
